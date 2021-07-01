ded20d4e5eb3cadc9660e24f148a8bae58f834c7
K TARUN <84392489+Taruntesla@users.noreply.github.com>
2021-07-01 17:33:09 +0530

    sol2

--- swap.c
+++ swap.c
@@ -1,21 +1,20 @@
 void swap_max(int arr[],int l,int n){
 int k=0;
 	int c= arr[n];
-	for(int i=n;i<l;i++){
+	for(int i=n+1;i<l;i++){
 		if(arr[i]>c){
 			c=arr[i];
-			k=i;
-			}
-			if(i==l-1){
-				int p=arr[n];
-				arr[n]=arr[k];
-				arr[k]=p;
+			k=i;  }
+	        if(i==l-1){
+	               int p=arr[n];
+		       arr[n]=arr[k];
+		       arr[k]=p;
 				}
 		}
     }
     
-    void ssort(int arr[],int l){
+    void ssort(int m[],int l){
     for(int i=0;i<l,i++){
-    swap_max(arr[],l,i);
+    swap_max(m,l,i);
     }
     }
